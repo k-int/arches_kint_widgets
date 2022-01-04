d192b45e97308cdc49ed25558e5c6e9acd3f2b8f
KacperSzyf <kacper.szyf@icloud.com>
2022-01-04 17:13:24 +0000

    fixed js

--- youtube-widget/youtube-video.js
+++ youtube-widget/youtube-video.js
@@ -8,14 +8,35 @@ define(['knockout', 'underscore', 'viewmodels/widget'], function (ko, _, WidgetV
     * @param {string} params.config().label - label to use alongside the text input
     * @param {string} params.config().placeholder - default text to show in the text input
     */
-    return ko.components.register('youtube-video', {
+     return ko.components.register('youtube-video', {
         viewModel: function(params) {
             // params.configKeys = ['x_placeholder','y_placeholder'];
+            params.valueProperties = ['url'];
+            console.log("Printing params:\n")
+            console.log(params)
             WidgetViewModel.apply(this, [params]);
-            var self = this;
-            self.YoutubeUrl = self.value();
-            // https://www.youtube.com/watch?v=ArCLT4ds0Yc&ab_channel=ArchesProject -> https://www.youtube.com/embed/ArCLT4ds0Yc
+            var self = this
+            console.log("Printing self:\n")
+            console.log(self)
+            
+            console.log('url:' + self.url())
+            
+
+            self.YoutubeUrl = self.url();
+            if(self.YoutubeUrl !== null){
+            // https://www.youtu.be/ArCLT4ds0Yc -> https://www.youtube.com/embed/ArCLT4ds0Yc
+            start = "https://www.youtube.com/embed/"
+            slash_split = self.YoutubeUrl.split("/")
+            video_code = slash_split[slash_split.length - 1]
+            end = start+video_code 
+            self.YoutubeUrl = end
+        };
+
+
             console.log(self.YoutubeUrl);
+
+            
+
         },
         template: { require: 'text!templates/views/components/widgets/youtube-video.htm' }
     });
