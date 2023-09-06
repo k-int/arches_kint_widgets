aeac9eae2f47af47be3098c6963ac03bcb00e8ea
SDScandrettKint <samuel.scandrett@k-int.co.uk>
2023-09-06 12:45:08 +0100

    update youtube urls

--- youtube-widget/youtube-video.js
+++ youtube-widget/youtube-video.js
@@ -1,37 +1,42 @@
 define(["knockout", "underscore", "viewmodels/widget"], function (
-    ko,
-    _,
-    WidgetViewModel
-  ) {
-    /**
-     * registers a text-widget component for use in forms
-     * @function external:"ko.components".text-widget
-     * @param {object} params
-     * @param {string} params.value - the value being managed
-     * @param {function} params.config - observable containing config object
-     * @param {string} params.config().label - label to use alongside the text input
-     * @param {string} params.config().placeholder - default text to show in the text input
-     */
-  
-    return ko.components.register("youtube-video", {
-      viewModel: function (params) {
-        WidgetViewModel.apply(this, [params]);
-        var that = this;
-        
-        //Bind YoutubeUrl to entered short url
-        that.YoutubeUrl = that.value();
-  
-        if (that.YoutubeUrl !== null) {
-          // https://www.youtu.be/ArCLT4ds0Yc --> https://www.youtube.com/embed/ArCLT4ds0Yc
-          startUrl = "https://www.youtube.com/embed/";
-          splitUrl = that.YoutubeUrl.split("/");
-          videoId = splitUrl[splitUrl.length - 1];  //Take just the video ID of entered url
-          that.YoutubeUrl = startUrl + videoId;     //Combine embed link with video ID
-        }
-      },
-      template: {
-        require: "text!templates/views/components/widgets/youtube-video.htm",
-      },
-    });
+  ko,
+  _,
+  WidgetViewModel
+) {
+  /**
+   * registers a text-widget component for use in forms
+   * @function external:"ko.components".text-widget
+   * @param {object} params
+   * @param {string} params.value - the value being managed
+   * @param {function} params.config - observable containing config object
+   * @param {string} params.config().label - label to use alongside the text input
+   * @param {string} params.config().placeholder - default text to show in the text input
+   */
+
+  return ko.components.register("youtube-video", {
+    viewModel: function (params) {
+      WidgetViewModel.apply(this, [params]);
+      var that = this;
+      
+      //Bind YoutubeUrl to entered short url
+      that.YoutubeUrl = that.value();
+
+      if (that.YoutubeUrl !== null) {
+        // e.g. https://www.youtu.be/ArCLT4ds0Yc --> https://www.youtube.com/embed/ArCLT4ds0Yc
+        // multiple types of youtube video URLs https://gist.github.com/rodrigoborgesdeoliveira/987683cfbfcc8d800192da1e73adc486
+
+        startUrl = "https://www.youtube.com/embed/";
+
+        // regex to match most variations of youtube URLs
+        // https://gist.github.com/rodrigoborgesdeoliveira/987683cfbfcc8d800192da1e73adc486?permalink_comment_id=4342805#gistcomment-4342805
+        const regexpUrlMatch = /(?<=[=\/&])[a-zA-Z0-9_\-]{11}(?=[=\/&?#\n\r]|$)/
+        const extractedID = that.YoutubeUrl.match(regexpUrlMatch)[0]
+
+        that.YoutubeUrl = startUrl + extractedID;     // Combine embed link with video ID
+      }
+    },
+    template: {
+      require: "text!templates/views/components/widgets/youtube-video.htm",
+    },
   });
-  
+});
