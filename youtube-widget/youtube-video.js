20fab9855392c4c6640ce4fb868e684075d4277b
CWDamm-Kint <chris.damm@k-int.co.uk>
2024-10-17 12:13:18 +0100

    fix youtube widget to work with 7.6

--- youtube-widget/youtube-video.js
+++ youtube-widget/youtube-video.js
@@ -1,7 +1,9 @@
-define(["knockout", "underscore", "viewmodels/widget"], function (
+define(["knockout", "underscore", "viewmodels/widget", "arches", "templates/views/components/widgets/youtube-video.htm"], function (
   ko,
   _,
-  WidgetViewModel
+  WidgetViewModel,
+  arches,
+  youTubeWidgetTemplate
 ) {
   /**
    * registers a text-widget component for use in forms
@@ -13,30 +15,31 @@ define(["knockout", "underscore", "viewmodels/widget"], function (
    * @param {string} params.config().placeholder - default text to show in the text input
    */
 
-  return ko.components.register("youtube-video", {
-    viewModel: function (params) {
+    const viewModel = function (params) {
       WidgetViewModel.apply(this, [params]);
       var that = this;
       
       //Bind YoutubeUrl to entered short url
       that.YoutubeUrl = that.value();
-
+      
       if (that.YoutubeUrl !== null) {
         // e.g. https://www.youtu.be/ArCLT4ds0Yc --> https://www.youtube.com/embed/ArCLT4ds0Yc
         // multiple types of youtube video URLs https://gist.github.com/rodrigoborgesdeoliveira/987683cfbfcc8d800192da1e73adc486
-
+        
         startUrl = "https://www.youtube.com/embed/";
-
+        
         // regex to match most variations of youtube URLs
         // https://gist.github.com/rodrigoborgesdeoliveira/987683cfbfcc8d800192da1e73adc486?permalink_comment_id=4342805#gistcomment-4342805
         const regexpUrlMatch = /(?<=[=\/&])[a-zA-Z0-9_\-]{11}(?=[=\/&?#\n\r]|$)/
-        const extractedID = that.YoutubeUrl.match(regexpUrlMatch)[0]
+        const extractedID = that.YoutubeUrl.match(regexpUrlMatch)
 
-        that.YoutubeUrl = startUrl + extractedID;     // Combine embed link with video ID
+        extractedID ? that.YoutubeUrl = startUrl + extractedID[0] : that.YoutubeUrl = startUrl // Combine embed link with video ID
       }
-    },
-    template: {
-      require: "text!templates/views/components/widgets/youtube-video.htm",
-    },
+    };
+
+    return ko.components.register("youtube-video", {
+        viewModel: viewModel,
+        template: youTubeWidgetTemplate
   });
 });
+
